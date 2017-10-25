7c9b1b91bbcc0fcc645b2e8aecc26d7045553be4
Irwin Li <izl2000@columbia.edu>
2017-10-24 21:18:35 -0400

    Not quite working logging

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 
-from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify, url_for
 from flask_pymongo import PyMongo
+import bcrypt
 app = Flask(__name__)
 
 
@@ -20,11 +21,39 @@ def create_account():
 
 @app.route("/home", methods=["GET"])
 def home():
-    return render_template("home.html")
+    if "username" in session:
+        return render_template("home.html")
+    return redirect(url_for("index"))
+
+@app.route("/register", methods=["POST"])
+def register():
+    users = mongo.db.users
+    existing_user = users.find_one({'name' : request.form['username']})
+    if existing_user is None:
+        if request.form['password'] == request.form['confirmPassword']:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for("home"))
+        else:
+            return "ERROR: Passwords do not match"
+    
+    return "ERROR: Username already exists"
 
 @app.route("/login", methods=["POST"])
 def login():
-    # TODO: Everything
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+    print(login_user)
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            print("made it???")
+            return redirect(url_for('home'))
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    app.secret_key = 'mysecret'
     app.run(host="0.0.0.0", port=8000)
